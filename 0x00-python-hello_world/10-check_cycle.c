cdc787a5c3cda9ff62a9791f8b9e098f00d44710
bryano13 <bryano13@hotmail.com>
2020-04-27 22:47:32 -0500

    10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if there is a cycle
+ * @list: list to examinate
+ * Return: 1 if true, 0 if false
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *head, *current;
+
+	head = list;
+	current = list;
+
+	while (head && current && current->next)
+	{
+		current = current->next->next;
+		if (head == current)
+			return (1);
+
+		head = head->next;		
+	}
+
+	return (0);
+}
