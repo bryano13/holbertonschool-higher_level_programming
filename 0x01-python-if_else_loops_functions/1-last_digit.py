92916febc3d86a29f0abe10909f3f5f6c8bbbf5e
bryano13 <bryano13@hotmail.com>
2020-04-28 19:43:41 -0500

    1,2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
-def uppercase(str):
+import random
+number = random.randint(-10000, 10000)
+
+if number < 0:
+    ldigit = (((number * -1) % 10) * -1)
+else:
+    ldigit = number % 10
+
+print("Last digit of {0:d} is {1:d} and is".format(number, ldigit), end=" ")
+if ldigit > 5:
+    print("greater than 5")
+elif ldigit < 6 and ldigit is not 0:
+    print("less than 6 and not 0")
+elif ldigit == 0:
+    print("0")
