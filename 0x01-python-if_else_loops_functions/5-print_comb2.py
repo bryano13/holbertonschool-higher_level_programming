0d605e8de7e43b2ec155eff6215a758b037a4565
bryano13 <bryano13@hotmail.com>
2020-04-28 22:43:18 -0500

    5d

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(0,99):
-	print("{:0>2d},".format(i), end = " ")
-print("99")
+for number in range(0,99):
+	print("{:0>2d},".format(number), end = " ")
+print(number + 1)
