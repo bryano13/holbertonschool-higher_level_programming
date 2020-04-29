df3e74cb9e38e1098e9d54c4d9a67253e4d63a12
bryano13 <bryano13@hotmail.com>
2020-04-29 02:55:33 -0500

    9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def print_last_digit(number):   
-    if number < 0:
-        digit = (number * -1) % 10
+def print_last_digit(number):
+    if (number < 0):
+        last_digit = -1 * number % 10
     else:
-        digit = number % 10
-    print(digit, end="")
-    return digit
+        last_digit = number % 10
+    print(last_digit, end="")
+    return (last_digit)
