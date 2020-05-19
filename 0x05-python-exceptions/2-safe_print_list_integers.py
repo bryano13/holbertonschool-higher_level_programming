3edbe3002aff2de644a0e7a76154c9d64aca5551
bryano13 <bryano13@hotmail.com>
2020-05-18 23:47:39 -0500

    all folder

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(0, x):
+            if type(my_list[i]) is int:
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+    except TypeError:
+        pass
+    print()
+    return(count)
