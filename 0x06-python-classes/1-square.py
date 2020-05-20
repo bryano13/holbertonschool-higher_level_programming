b3046c47330b1d3bcbaa8ed3e20f03b59d808550
bryano13 <bryano13@hotmail.com>
2020-05-19 22:54:00 -0500

    first commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module creates a class Square"""
+
+
+class Square:
+    """ Class Square with a constuctor method"""
+
+    def __init__(self, size):
+        """
+        Initializes square
+        Args:
+            size: size for __size attribute
+        """
+        self.__size = size
