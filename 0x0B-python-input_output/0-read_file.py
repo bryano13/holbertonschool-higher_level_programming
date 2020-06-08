c237ef01bf3d078911494956817994b19b8cb85d
bryano13 <bryano13@hotmail.com>
2020-06-07 23:30:31 -0500

    0 optimized

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,5 @@ def read_file(filename=""):
     Args:
         filename (str): Filename
     """
-    with open(filename, encoding="utf-8") as f:
-        print(f.read())
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read())
