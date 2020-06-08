e4835928905b7ae107015253b282e0b367877afc
bryano13 <bryano13@hotmail.com>
2020-06-08 01:58:38 -0500

    4

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""wirte to a file"""
+"""Module that appends text to a file"""
 
 
 def append_write(filename="", text=""):
-    """write to a file"""
-
-    with open(filename, 'a') as f:
-        char = f.write(text)
-
-    return char
+    with open(filename, "a", encoding="utf-8") as file:
+        file.write(text)
+    return len(text)
