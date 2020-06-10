5949e1a3a5169b37cf999ab00c7c3eb4a4326d2d
bryano13 <bryano13@hotmail.com>
2020-06-09 22:00:39 -0500

    models pep8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -87,7 +87,11 @@ class Rectangle(Base):
     def display(self):
         """Method that displays the rectangle in hashtags #"""
 
+        for y in range(0, self.__y):
+            print()
         for i in range(0, self.__height):
+            for x in range(0, self.__x):
+                print(" ", end="")
             for j in range(0, self.__width):
                 print("#", end="")
             print()
