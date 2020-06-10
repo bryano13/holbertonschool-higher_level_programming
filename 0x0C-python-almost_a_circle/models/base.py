585af4c0ebe00c6fd9556410e943f0a96401c032
bryano13 <bryano13@hotmail.com>
2020-06-09 20:43:35 -0500

    all folder

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This class will be the “base” of all other classes in this project.
+The goal of it is to manage id attribute in all your future classes
+and to avoid duplicating the same code (by extension, same bugs)
+"""
+
+
+class Base():
+    """Base object template"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Base init for object"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+"""Class Rectangle that inherits from class Base"""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Class rectangle"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialization of instances"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    # Getter and setter for width
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("widht must be > 0")
+        self.__width = value
+    # --------------------------------------------------
+
+    # Getter and setter for height
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+    # ------------------------------------------------------
+
+    # Getter and Setter for x
+    @property
+    def x(self):
+        """x getter"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """x setter"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be > 0")
+        self.__x = value
+    # --------------------------------------------------------
+
+    # Getter and Setter for y
+    @property
+    def y(self):
+        """y getter"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """y setter"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be > 0")
+        self.__y = value
+    # ---------------------------------------------------------
+
+    # area method
+    def area(self):
+        """Method that calculates the area of Rectangle"""
+        return self.__width * self.__height
+
