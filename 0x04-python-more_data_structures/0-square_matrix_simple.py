287bef8cf22f17806cae875cf806b2ed32b96648
bryano13 <bryano13@hotmail.com>
2020-06-13 19:32:56 -0500

    0 fixed

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+
+
 def square_matrix_simple(matrix=[]):
-    list1 = []
-    for j in matrix:
-        list1.append([i**2 for i in j])
-    return list1
+    """Function that returns a list of
+    the square values of another list"""
+
+    copy = matrix[:]
+    index = 0
+    for nlist in copy:
+        new_list = nlist[:]
+        for i in range(len(nlist)):
+            new_list[i] = new_list[i] ** 2
+        copy[index] = new_list
+        index += 1
+    return copy
