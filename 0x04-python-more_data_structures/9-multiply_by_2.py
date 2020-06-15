973894c76a87c20bac1426f1cb41d9d419f6ef19
bryano13 <bryano13@hotmail.com>
2020-06-15 00:38:03 -0500

    9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
+
 def multiply_by_2(a_dictionary):
-    new_dict = {}
-    for i in a_dictionary:
-        new_dict[i] = a_dictionary[i] * 2
+    new_dict = a_dictionary.copy()
+    for key in a_dictionary:
+        new_dict[key] = a_dictionary[key] * 2
     return new_dict
