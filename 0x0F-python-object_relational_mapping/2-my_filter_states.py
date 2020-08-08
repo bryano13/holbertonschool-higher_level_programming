89d4400c6b4f9bb8704cf50833c40d5aaceecd8d
bryano13 <bryano13@hotmail.com>
2020-08-08 12:00:45 -0500

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""List all the states from a database beginning by N
+ protected of SQL injection"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    u = argv[1]
+    p = argv[2]
+    d = argv[3]
+    n = argv[4]
+    # connect to the database
+    db = MySQLdb.connect(host="localhost", user=u, passwd=p, db=d, port=3306)
+    # get working environment with cursor object
+    cur = db.cursor()
+    stmt = "SELECT * \
+            FROM states \
+            WHERE BINARY name = '{}' \
+            ORDER BY id ASC".format(n)
+
+    cur.execute(stmt)
+    # get all records of the query
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
