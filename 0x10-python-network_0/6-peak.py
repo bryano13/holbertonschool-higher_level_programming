bba70ed85984e9aa52265b49b2fc5fa6181cc52b
bryano13 <bryano13@hotmail.com>
2020-08-18 21:18:34 -0500

    task 6 efficient

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-""" sends a request to a URL passed as an argument,
-    and displays only the status code of the response """
+"""
+Finds peaks using python
+"""
 
 
 def find_peak(list_of_integers):
-    """Find peak in list"""
-    listt = list_of_integers.copy()
-    for idx in range(len(listt)):
-        if (idx - 1) >= 0 and (idx + 1) <= (len(listt) - 1):
-            if listt[idx] >= listt[idx - 1] and listt[idx] >= listt[idx + 1]:
-                return listt[idx]
-        if (idx - 1) < 0:
-            if listt[idx] >= listt[idx + 1]:
-                return listt[idx]
-        if (idx + 1) > (len(listt) - 1):
-            if listt[idx] >= listt[idx - 1]:
-                return listt[idx]
-    return None
+    """
+    This function returns the peak of a list
+    """
+    if list_of_integers:
+        if(list_of_integers[0] > list_of_integers[1]):
+            return list_of_integers[0]
+        elif(list_of_integers[-1] > list_of_integers[-2]):
+            return list_of_integers[-1]
+        else:
+            return(max(list_of_integers))
+    return(None)
