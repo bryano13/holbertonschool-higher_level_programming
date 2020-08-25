61bf74f787720cb5d5404b6c0a837bc25ecf4fe3
bryano13 <bryano13@hotmail.com>
2020-08-24 23:25:31 -0500

    4 nd 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""
-takes in a URL and an email, sends a POST
-request to the passed URL with the email as a parameter,
-and displays the body of the response (decoded in utf-8)
-"""
+"""This modules defines a task"""
 
 import requests
+from sys import argv
 
-if __name__ == '__main__':
-    URL = 'https://intranet.hbtn.io/status'
-    req = requests.get(URL)
-    print('Body response:')
-    print('\t- type: {}'.format(type(req.text)))
-    print('\t- content: {}'.format(req.text))
+
+if __name__ == "__main__":
+    req = requests.get(argv[1])
+    print(req.headers.get('X-Request-Id'))
