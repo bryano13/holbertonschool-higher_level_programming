6517c3687bcd8f43066286c7d5036ebc1e821f25
bryano13 <bryano13@hotmail.com>
2020-08-24 23:28:24 -0500

    6-8

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""This modules defines a task"""
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    req = requests.post(argv[1], data={'email': argv[2]})
+    print(req.text)
