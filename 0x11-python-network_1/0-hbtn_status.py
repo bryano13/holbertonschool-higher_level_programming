14a280457cdaa3ff44e36892e9722715e64cdacb
bryano13 <bryano13@hotmail.com>
2020-08-24 23:22:39 -0500

    fixed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """
-This module creates a request in the
-internet. Fetches https://intranet.hbtn.io/status
+fetches https://intranet.hbtn.io/status
 """
 
 import urllib.request
-
 
 if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
@@ -13,4 +11,4 @@ if __name__ == '__main__':
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
         print('\t- content: {}'.format(html))
-        print('\t utf8 content: {}'.format(html.decode('utf8')))
+        print('\t- utf8 content: {}'.format(html.decode('utf8')))
