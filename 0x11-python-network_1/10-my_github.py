28b0f3fd6ac8dbfb7f0c9b2a6619e5c6f5d215ef
bryano13 <bryano13@hotmail.com>
2020-08-24 23:31:01 -0500

    10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""This modules defines a task"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    username = argv[1]
+    password = argv[2]
+    try:
+        req = requests.get('https://api.github.com/user', auth=(
+            argv[1], argv[2])).json()
+        print(req.get('id'))
+    except:
+        print("None")
