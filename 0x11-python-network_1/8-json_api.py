e4bfa5d9f5f9109f279955ee3008f069d552a407
bryano13 <bryano13@hotmail.com>
2020-08-25 23:33:07 -0500

    8 fixed

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
-""" script that takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter."""
+"""Script that takes a letter and sends a 
+POST request to a URL with the letter as a 
+parameter"""
 
 import requests
 from sys import argv
 
 
 if __name__ == "__main__":
-
     if len(argv) > 1:
-        data = {'q': argv[1]}
+        data = {"q": argv[1]}
     else:
-        data = {'q': ""}
-    # Send post request
-    res = requests.post('http://0.0.0.0:5000/search_user', data)
-    try:
-        json = res.json()
-        if json:
-            print("[{}] {}".format(
-                                   json['id'],
-                                   json['name']))
+        data = {"q": ""}
+    url = "http://0.0.0.0:5000/search_user"
+    response = requests.post(url, data)
+    dict1 = response.json()
+    if type(dict1) == dict:
+        if dict1:
+            print("[{}] {}".format(dict1["id"], dict1["name"]))
         else:
             print("No result")
-    except Exception:
+    else:
         print("Not a valid JSON")
