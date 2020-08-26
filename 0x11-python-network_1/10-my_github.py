ff93011671aeb5b52712f60ac2627bd72e0392f4
bryano13 <bryano13@hotmail.com>
2020-08-26 01:26:23 -0500

    10 fixed

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""This modules defines a task"""
+"""Script that authenticates a github user and
+returns the id of the user"""
 
 import requests
 from sys import argv
 
-if __name__ == "__main__":
 
-    username = argv[1]
-    password = argv[2]
+if __name__ == "__main__":
+    if len(argv) >= 3:
+        user = argv[1]
+        password = argv[2]
+    else:
+        user = ""
+        password = ""
+
+    response = requests.get(
+        'https://api.github.com/user', auth=(user, password))
     try:
-        req = requests.get('https://api.github.com/user', auth=(
-            argv[1], argv[2])).json()
-        print(req.get('id'))
-    except:
+        my_dict = response.json()
+        print(my_dict["id"])
+    except Exception:
         print("None")
