49ec6ee99b906ffc216816f107599bd67b38880d
bryano13 <bryano13@hotmail.com>
2020-09-15 18:53:02 -0500

    2, 3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+/*
+Script that prints the first argument passed to it
+*/
+const args = process.argv;
+if (args[2]) {
+  console.log(args[2]);
+} else {
+  console.log('No argument');
+}
