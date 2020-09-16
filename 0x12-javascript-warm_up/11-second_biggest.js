3c972ce6d075e4ac544fd4fdb3f64c5c22a1a977
bryano13 <bryano13@hotmail.com>
2020-09-15 19:27:22 -0500

    11-13

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+// searches the second biggest integer in the list of arguments
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const array = process.argv.slice(2).sort((a, b) => a - b);
+  console.log(array[array.length - 2]);
+}
