979af2645ed3b9b264ae38dca0510d54142cc83f
bryano13 <bryano13@hotmail.com>
2020-09-15 19:14:21 -0500

    2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-//task 2
-if (process.argv.length === 2) {
-    console.log('No argument');
-  } else if (process.argv.length === 3) {
-    console.log('Argument found');
-  } else {
-    console.log('Arguments found');
-  }
+if (!process.argv[2]) {
+  console.log('No argument');
+} else if (process.argv[2] && !process.argv[3]) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
